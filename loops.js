11828e575c5619cdddfb4c193c94095ed3699fab
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-15 13:21:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,13 +18,14 @@ function whileLoop(n) {
     return "done";
   }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5;
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+var i = 0
+function incrementVariable() {
+  i = i + 1;
+  return i;
 }
+
+function doWhileLoop(integer){
+  do{
+    console.log("I run once regardless.")
+  } while (incrementVariable() <= integer)
+} 
