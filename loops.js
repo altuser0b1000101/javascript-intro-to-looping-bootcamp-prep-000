77521c0cb9e6b87bb90362ecde8ce88902cdfd88
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-15 12:59:03 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push(`I am 1 strange loop.`);
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr;
+}
+const arr = [];
+const out = forLoop(arr);
+console.log(out);
+
+//strange(2)
+
+function whileLoop(number) {
+while (number > 0) {
+console.log(–number);
+}
+return “done”;
+}
